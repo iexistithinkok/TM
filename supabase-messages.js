364a9d693f818ef686ec7164b99b2dfcefca7e27
iexistithinkok <iexistithink@yahoo.com>
2026-01-13 17:59:36 -0500

    Update supabase-messages.js

--- supabase-messages.js
+++ supabase-messages.js
@@ -6,6 +6,12 @@
   const clientFeed = document.querySelector("[data-client-message-feed]");
   const adminFeed = document.querySelector("[data-admin-message-feed]");
 
+  console.log("supabase-messages loaded", {
+    hasClientFeed: !!clientFeed,
+    hasAdminFeed: !!adminFeed,
+    path: window.location.pathname,
+  });
+
   if (clientFeed) clientFeed.insertAdjacentHTML("beforeend", "<p class='muted'>Client feed ready.</p>");
   if (adminFeed) adminFeed.insertAdjacentHTML("beforeend", "<p class='muted'>Admin feed ready.</p>");
 })();
