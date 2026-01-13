a7cc85c9fa6c67ec86d3db61f793659abf5c87ad
iexistithinkok <iexistithink@yahoo.com>
2026-01-13 15:41:40 -0500

    Update Supabase ANON_KEY in supabase-env.js

--- supabase-env.js
+++ supabase-env.js
@@ -2,4 +2,4 @@
 // Example for Cloudflare Pages: set SUPABASE_URL and SUPABASE_ANON_KEY env vars
 // and replace the placeholders in this file during deployment.
 window.__SUPABASE_URL__ = "https://odbcpoxlapcxzwiqjmyw.supabase.co";
-window.__SUPABASE_ANON_KEY__ = "c98a9520-d4f9-464e-853f-3b7fde715891";
+window.__SUPABASE_ANON_KEY__ = "sb_publishable_uPFq7zPYedEfaMOdleSSKg_LvSowQE-";
